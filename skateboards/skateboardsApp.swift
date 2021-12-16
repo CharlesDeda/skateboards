3f6b65042ecc4d3c0d4b866e9815cf713cb25eb4
CharlesDeda <94919280+CharlesDeda@users.noreply.github.com>
2021-12-16 15:29:37 -0500

    Initial Commit

---
+++ skateboards/skateboardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  skateboardsApp.swift
+//  skateboards
+//
+//  Created by Nick Deda on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct skateboardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
