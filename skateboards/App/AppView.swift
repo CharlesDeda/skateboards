0acdd86b7762a7c16ad80ca64ab8de38daa50021
CharlesDeda <94919280+CharlesDeda@users.noreply.github.com>
2021-12-16 16:38:03 -0500

    Cleanup Files

---
+++ skateboards/App/AppView.swift
@@ -0,0 +1,23 @@
+//
+//  appView.swift
+//  skateboards
+//
+//  Created by Nick Deda on 12/16/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppView: View {
+  let skateboard = mock
+  var body: some View {
+    MainSkateboardView(skateboard: skateboard)
+  }
+}
+
+struct AppView_Previews: PreviewProvider {
+  static var previews: some View {
+    AppView()
+      .preferredColorScheme(.dark)
+  }
+}
--- skateboards/Skateboard/SkateboardView.swift
+++ skateboards/Skateboard/SkateboardView.swift
@@ -22,9 +22,9 @@ let mock = Skateboard(
   description: .lorumIpsum
 )
 
-struct AppView: View {
-  let skateboard = mock
-  
+struct MainSkateboardView: View {
+  let skateboard: Skateboard
+
   var body: some View {
     VStack(alignment: .leading) {
       VStack(alignment: .leading) {
@@ -92,13 +92,5 @@ struct WheelsView: View {
     }
   }
 }
-extension String {
-  static let lorumIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
 
-struct AppView_Previews: PreviewProvider {
-  static var previews: some View {
-    AppView()
-      .preferredColorScheme(.dark)
-  }
-}
+
